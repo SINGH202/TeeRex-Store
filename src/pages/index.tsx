e08393e7e0ae31566da5e3450aabaed4f261cac8
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-08-02 00:12:59 +0530

    Added product card.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Navbar } from "@/components/Navbar";
+import { ProductCard } from "@/components/ProductCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center border w-full ${inter.className}`}>
       <Navbar />
+      {/* <ProductCard /> */}
     </main>
   );
 }

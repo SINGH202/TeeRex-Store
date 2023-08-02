9af1e36ea009dd85d3144e698ebf7c17d1a5ee79
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-08-03 01:03:37 +0530

    Added route for cart page.

---
+++ src/pages/cart.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "@/components/Navbar";
+
+export default function Cart() {
+  return (
+    <main className={`flex min-h-screen flex-col items-center border w-full`}>
+      <Navbar />
+      {/* <ProductCard /> */}
+    </main>
+  );
+}

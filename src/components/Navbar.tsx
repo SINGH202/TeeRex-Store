2c9078d39659ce9ccc4f13f578bdee944599d130
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-08-04 00:13:24 +0530

    Added routes to navItems.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export const Navbar = () => {
+  const router = useRouter();
   return (
     <div className="flex items-center justify-between font-medium text-xl h-20 px-5 sm:px-16 w-full bg-[#F1F0F0]">
       <span className="">TeeRex Store</span>
       <div className="flex items-center justify-end gap-10">
-        <span>Products</span>
-        <div className="bg-[#D3D3D3] p-3 rounded-md relative">
+        <span
+          className="cursor-pointer"
+          onClick={() => {
+            router.push("/");
+          }}>
+          Products
+        </span>
+        <div
+          className="bg-[#D3D3D3] cursor-pointer p-3 rounded-md relative"
+          onClick={() => {
+            router.push("cart");
+          }}>
           <span className="absolute text-sm top-1 right-1">1</span>
           <Image
             width={30}

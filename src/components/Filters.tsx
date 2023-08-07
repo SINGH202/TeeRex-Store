91a9b50d75a97c9f2c933c5dc6bdad9eafa28686
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-08-08 00:12:43 +0530

    Added filter option design.

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,3 +1,32 @@
 export const Filters = () => {
-  return <div className="border border-black"></div>;
+  return (
+    <div className="font-semibold hidden md:flex flex-col items-start gap-4 md:w-96 border px-6 py-5 shadow-lg rounded-md mt-20">
+      <span className="text-lg">Colour</span>
+      <div className="flex w-full gap-5 items-center">
+        <input
+          type="checkbox"
+          name=""
+          id=""
+          className="bg-gray-400 accent-[#50d71e]"
+        />
+        <span>Red</span>
+      </div>
+    </div>
+  );
+};
+
+
+
+export const FilterOptions = () => {
+  return (
+    <div className="flex w-full gap-5 items-center">
+      <input
+        type="checkbox"
+        name=""
+        id=""
+        className="bg-gray-400 accent-[#50d71e]"
+      />
+      <span>Red</span>
+    </div>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { Navbar } from "@/components/Navbar";
 import { ProductCard } from "@/components/ProductCard";
 import { SearchBar } from "@/components/SearchBar";
+import { Filters } from "@/components/Filters";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,8 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center border w-full ${inter.className}`}>
       <Navbar />
-      <div className="flex items-start justify-between gap-20 border border-black p-10 w-full">
-        <div className="hidden md:flex flex-col md:w-96 border px-6 py-5 shadow-lg rounded-md"></div>
+      <div className="flex items-start justify-between gap-20 p-10 w-full">
+        <Filters />
         <div className="flex flex-col items-center gap-10 w-full border border-black">
           <SearchBar />
           <div className="w-full flex flex-wrap justify-center gap-x-10 gap-y-16">

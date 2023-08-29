7e282c08fb6fec1ef4dcc01b29cb4ca3c49c4aee
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-08-30 00:03:47 +0530

    Added cart card designs for responsive screens.

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -7,19 +7,19 @@ export default function Cart() {
       <Navbar />
       <div className="flex flex-col gap-10 w-full max-w-7xl my-10 px-5 m">
         <span className="text-3xl font-medium">Shopping Cart</span>
-        <div className="flex flex-col gap-5 max-w-3xl px-5 lg:mx-28">
+        <div className="flex flex-col gap-5 max-w-3xl sm:px-5 lg:mx-28">
           {Array.from({ length: 3 }).map((_, index) => (
             <div
               key={index}
-              className="flex flex-col sm:flex-row items-center justify-between gap-10 py-2 border">
-              <div className="flex items-center justify-between gap-10 py-2">
+              className="flex flex-col sm:flex-row items-center justify-between gap-10 p-2">
+              <div className="flex items-center justify-between gap-10 py-2 w-full sm:w-fit">
                 <div className="bg-[#d7d7d7] w-24 h-24"></div>
                 <div className="flex flex-col justify-center gap-1">
                   <p className="font-bold text-2xl">Pink Round</p>
                   <p className="font-bold text-lg">Pink Round</p>
                 </div>
               </div>
-              <div className="flex justify-between items-end gap-5">
+              <div className="flex justify-between sm:justify-start w-full sm:w-fit sm:items-end gap-5">
                 <QuantityDropdown />
                 <button className="text-lg border-[1.5px] border-black rounded-md px-6 py-1.5 w-[130px]">
                   Delete
